6663c36c4837de1490fc5e89ffc0181a6df40073
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-13 13:05:44 +0300

    Finished function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(roll = 'roller-skate') {
+    return (`This Saturday, I want to ${roll}!`);
+  }
+console.log(saturdayFun(`bathe my dog`))
+console.log(saturdayFun())
+
+function mondayWork(work = 'go to the office') {
+  return`This Monday, I will ${work}.`;
+}
+console.log(mondayWork(` work from home.`))
+console.log(mondayWork())
+
+function wrapAdjective(symbol = "*") {
+  return function(adjective = "a hard worker") {
+    return `You are ${symbol}${adjective}${symbol}!`;
+  };
+}
+
+console.log(wrapAdjective('||')('a dedicated programmer'));
+console.log(wrapAdjective()());
